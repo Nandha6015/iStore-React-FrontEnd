4d0bb2171bb3cceac16478deb68ee0975b832d56
mantharagirimuthu <mantharagiri_muthu@thbs.com>
2022-01-25 18:36:22 +0530

    inbox

--- src/component/contact/contactinbox.js
+++ src/component/contact/contactinbox.js
@@ -1,5 +1,22 @@
 import React from "react";
 import "./contactinbox.css";
+const inbox = [
+  {
+    name: "giri",
+    message: "hai",
+    img: "https://bootdey.com/img/Content/avatar/avatar2.png",
+  },
+  {
+    name: "nandha",
+    message: "haiii",
+    img: "https://bootdey.com/img/Content/avatar/avatar7.png",
+  },
+  {
+    name: "ashraff",
+    message: "bye",
+    img: "https://bootdey.com/img/Content/avatar/avatar6.png",
+  },
+];
 const ContactInbox = () => {
   return (
     <div class="container pb-5 ">
@@ -8,22 +25,25 @@ const ContactInbox = () => {
           <h1>Admin Inbox</h1>
         </div>
       </div>
+
       <div class="row">
         <div class="conversation-wrap col-md-3">
-          <div class="media conversation">
-            <a class="pull-left" href="#">
-              <img
-                class="media-object img-circle"
-                id="style1"
-                src="https://bootdey.com/img/Content/user_2.jpg"
-              />
-            </a>
-            <div class="media-body">
-              <h5 class="media-heading">Michael clarkt</h5>
-              <small class="text-muted">Lorem ipsum dolor...</small>
+          {inbox.map((user) => (
+            <div class="media conversation">
+              <a class="pull-left" href="#">
+                <img
+                  class="media-object img-circle"
+                  id="style1"
+                  src={user.img}
+                />
+              </a>
+              <div class="media-body">
+                <h5 class="media-heading">{user.name}</h5>
+                <small class="text-muted">{user.message}</small>
+              </div>
             </div>
-          </div>
-
+          ))}
+          {/* 
           <div class="media conversation">
             <a class="pull-left" href="#">
               <img
@@ -92,7 +112,7 @@ const ContactInbox = () => {
               <h5 class="media-heading">Maria monthg</h5>
               <small class="text-muted">cursus ac...</small>
             </div>
-          </div>
+        </div> */}
         </div>
 
         <div class="message-wrap col-md-8">
